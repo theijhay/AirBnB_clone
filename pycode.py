d055a3baf37001efa213c4e7d0d1ade0bac37824
NathanielWatife <awosanminathaniel0@gmail.com>
2023-12-06 00:44:41 +0100

    new folders and file created

--- pycode.py
+++ pycode.py
@@ -1,8 +1,2 @@
-# These line of code is to past the pycodestyle
-
-myNum = 23
-
-if myNum > 50:
-    print("Access Denied..")
-else:
-    print("Access pycodestyle")
+#!/usr/bin/python3
+"""These line of code checks for pycodestyle pass"""
