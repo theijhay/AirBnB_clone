c8921bc2a45e82fc4e7e23594b4934f6344f7c52
NathanielWatife <awosanminathaniel0@gmail.com>
2023-12-06 02:39:27 +0100

    changes made in pycode.py

--- pycode.py
+++ pycode.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-"""These line of code checks for pycodestyle pass"""
+
+class ExampleClass:
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print(f"Hello, {self.name}!")
+
+
+def main():
+    example_instance = ExampleClass("World")
+    example_instance.print_name()
+
+
+if __name__ == "__main__":
+    main()
