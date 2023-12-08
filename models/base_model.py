dc05013f9e70c0a4676890b3b12a85640d97a8a4
theijhay <olawaleisaacjohn@gmail.com>
2023-12-08 09:16:37 +0100

    file storage added

--- models/base_model.py
+++ models/base_model.py
@@ -34,3 +34,18 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
