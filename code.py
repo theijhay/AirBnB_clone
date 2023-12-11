360be4a13c2cbc9f3fd07b56c27674da918aef5e
NathanielWatife <awosanminathaniel0@gmail.com>
2023-12-11 08:17:12 +0100

    changes made to code.py

--- code.py
+++ code.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
-"""Passes pycodestyle"""
+"""This is a sample Python script that adheres to PEP 8 style guide"""
 
 
-def do_add(self, s):
-    my_list = s.split()
-    if len(l) != 2:
-        print("*** invalid number of arguments")
-        return
-    try:
-        my_list = [int(i) for i in my_list]
-    except ValueError:
-        print("*** arguments should be numbers")
-        return
-    print(my_list[0]+my_list[1])
+class MyClass:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_info(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+
+def calculate_square(n):
+    """
+    Calculate the square of a number.
+
+    Args:
+        n (int): The number to be squared.
+
+    Returns:
+        int: The square of the input number.
+    """
+    return n ** 2
+
+
+def main():
+    """Create an instance of MyClass"""
+    obj = MyClass(name="Jane", age=10)
+
+    """Display information using the display_info method"""
+    obj.display_info()
+
+    """Calculate and print the square of 5"""
+    result = calculate_square(5)
+    print(f"The square of 5 is: {result}")
+
+
+if __name__ == "__main__":
+    main()
